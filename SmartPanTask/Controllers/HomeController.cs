d1b2ca312eefa78e696450ff1c5bb6c0e8f7aae3
ahmed anwar <ahmed.anwar.ibrahem66@gmail.com>
2019-05-09 19:58:16 +0200

    initialize the project

---
+++ SmartPanTask/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartPanTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //public ActionResult _Blog2()
+        //{
+        //    var CompanyInfo = db.Companies.Where(c => c.CompanyName == id).FirstOrDefault();
+        //    var companyId = db.Companies.Where(c => c.CompanyName == id).FirstOrDefault().UserId;
+        //    ViewBag.news = db.News.Where(c => c.Userid == companyId).OrderByDescending(a => a.Id).ToList();
+        //    return PartialView(CompanyInfo);
+        //}
+    }
+}
---
+++ SmartPanTask/Models/DisplayNameHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartPanTask.Models
+{
+
+
+
+     [MetadataType(typeof(EmployeeMetaData))]
+    public partial class Employee
+    {
+    }
+     public class EmployeeMetaData
+    {
+        [DisplayName( "First Name")]
+        [Required(ErrorMessage = "Please enter your first name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "Please enter your Last name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please enter your salary")]
+        public Nullable<decimal> Salary { get; set; }
+        public string Image { get; set; }
+        public Nullable<int> ManagerID { get; set; }
+        public Nullable<int> DepartmentID { get; set; }
+        public string UserId { get; set; }
+        public string Type { get; set; }
+    }
+
+}
